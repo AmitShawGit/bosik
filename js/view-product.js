9f4bd0003bd08d822974e3307f7daf80f83cd287
amit <amitshaw1421@gmail.com>
2025-06-08 22:39:45 +0530

    cart updated

--- js/view-product.js
+++ js/view-product.js
@@ -86,7 +86,7 @@ $(document).on('click', '.buy_now', function () {
         existingCart.push(parsedData);
         localStorage.setItem("addTocart", JSON.stringify(existingCart));
 
-        window.location.href = "/cart.html";
+        window.location.href = "https://amitshawgit.github.io/bosik/cart.html";
     } catch (e) {
         console.error("Failed to parse product data (buy_now):", e);
     }
